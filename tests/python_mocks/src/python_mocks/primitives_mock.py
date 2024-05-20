8502617f1951629e8376dc45bda716ba266be9a6
Existien <dummy@mail.de>
2024-05-20 15:18:12 +0200

    fix: missing return values for primitives mock

--- tests/python_mocks/src/python_mocks/primitives_mock.py
+++ tests/python_mocks/src/python_mocks/primitives_mock.py
@@ -5,33 +5,43 @@ def get_primitives_mock():
     service = BackendPrimitivesInterfaceMock()
     async def uint8_method_handler(value):
         service.Uint8Signal(value)
+        return value
     service.mock.Uint8Method = AsyncMock(wraps=uint8_method_handler)
     async def bool_method_handler(value):
         service.BoolSignal(value)
+        return value
     service.mock.BoolMethod = AsyncMock(wraps=bool_method_handler)
     async def int16_method_handler(value):
         service.Int16Signal(value)
+        return value
     service.mock.Int16Method = AsyncMock(wraps=int16_method_handler)
     async def uint16_method_handler(value):
         service.Uint16Signal(value)
+        return value
     service.mock.Uint16Method = AsyncMock(wraps=uint16_method_handler)
     async def int32_method_handler(value):
         service.Int32Signal(value)
+        return value
     service.mock.Int32Method = AsyncMock(wraps=int32_method_handler)
     async def uint32_method_handler(value):
         service.Uint32Signal(value)
+        return value
     service.mock.Uint32Method = AsyncMock(wraps=uint32_method_handler)
     async def int64_method_handler(value):
         service.Int64Signal(value)
+        return value
     service.mock.Int64Method = AsyncMock(wraps=int64_method_handler)
     async def uint64_method_handler(value):
         service.Uint64Signal(value)
+        return value
     service.mock.Uint64Method = AsyncMock(wraps=uint64_method_handler)
     async def double_method_handler(value):
         service.DoubleSignal(value)
+        return value
     service.mock.DoubleMethod = AsyncMock(wraps=double_method_handler)
     async def string_method_handler(value):
         service.StringSignal(value)
+        return value
     service.mock.StringMethod = AsyncMock(wraps=string_method_handler)
 
     return service
