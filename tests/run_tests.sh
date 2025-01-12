83c9321e3de9abac7ccac85c555c424443fe245c
Existien <dummy@mail.de>
2025-01-12 12:59:26 +0100

    chore: Improve github workflow

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,11 +7,6 @@ pushd $thisdir
 # Regenerate code
 ./generate.sh
 
-# Build software under test
-pushd qt6_service
-./build.sh
-popd
-
 # Install behave test requirements
 if [ ! -d "behave-tests/.venv" ];then
     python3 -m venv behave-tests/.venv
@@ -19,5 +14,20 @@ fi
 . behave-tests/.venv/bin/activate
 pip install -r behave-tests/requirements.txt
 
-# Start tests
-behave behave-tests
+if [[ $# == 0 ]];then
+    languages=(qt6 python)
+else
+    languages=$@
+fi
+
+# Run python tests
+if [[ ${languages[*]} =~ "python" ]];then
+    behave behave-tests/python
+fi
+
+if [[ ${languages[*]} =~ "qt6" ]];then
+    pushd qt6_service
+    ./build.sh
+    popd
+    behave behave-tests/qt6
+fi
